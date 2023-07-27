990670ecd1a191a0b6d420bf122e7ad48b58be8d
brianc <brianc@123>
2023-07-27 13:52:48 +0200

    added initial commit

---
+++ src/main/java/com/democompany/project_one/App.java
@@ -0,0 +1,13 @@
+package com.democompany.project_one;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
